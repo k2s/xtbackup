8fb14bba2d526e1cdeae463ff6c8754417620ab4
Martin Minka <martin.minka@gmail.com>
2014-04-08 22:14:42 +0200

    add shell script

---
+++ xtbackup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+php_bin=`which php`
+xtbackup_dir=`readlink -f $0`
+xtbackup_dir=`dirname $xtbackup_dir`
+
+cd "$xtbackup_dir"
+$php_bin -d open_basedir="/tmp:$xtbackup_dir" -f xtbackup.php -- $@

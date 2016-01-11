0126730150cca30ae386475d23cbacc3ea5b1b3d
Martin Minka <martin.minka@gmail.com>
2016-01-12 00:44:46 +0100

    disable open_basedir of env XTBACKUP_OPENDIR is empty

--- xtbackup.sh
+++ xtbackup.sh
@@ -5,4 +5,8 @@ xtbackup_dir=`readlink -f $0`
 xtbackup_dir=`dirname $xtbackup_dir`
 
 cd "$xtbackup_dir"
-$php_bin -d open_basedir="$XTBACKUP_OPENDIR:/tmp:$xtbackup_dir" -f xtbackup.php -- $@
+if [ -z $XTBACKUP_OPENDIR ]; then
+    $php_bin -d open_basedir="none" -f xtbackup.php -- $@
+else
+    $php_bin -d open_basedir="$XTBACKUP_OPENDIR:/tmp:$xtbackup_dir" -f xtbackup.php -- $@
+fi
